dcdce337a64edbee06b03c369f8099857172b3d2
tanmayb16 <68735911+tanmayb16@users.noreply.github.com>
2020-08-17 17:08:53 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,21 @@
+class Dustbin {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0,
+          'isStatic' : true,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      fill("yellow");
+      rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
